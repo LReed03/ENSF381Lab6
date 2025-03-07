ee6572f6e4c6406136c9091afba28d264b9e8eff
lreed03 <lreed0383@gmail.com>
2025-03-07 11:24:49 -0700

    part1

---
+++ jsx-exercise/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const year = new Date().getFullYear();
+
+  const isLoggedIn = true;
+  return (
+
+    <div>
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>${year}</p>
+      return isLoggedIn ?<p>Welcome back!</p> || <p>Please log in.</p>
+    </div>
+  );
+}
+
+export default App;
